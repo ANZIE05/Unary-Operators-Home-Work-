79ae0ecb2fb04edff10ddd97a3ac4562a8877726
ANZIE05 <161513339+ANZIE05@users.noreply.github.com>
2024-04-27 23:32:20 +0500

    Add files via upload
    Unary Operators / BEDMAS / Assignment Operators (Practice)

---
+++ index.js
@@ -0,0 +1,41 @@
+// UNARY OPERATORS (HOME WORK)
+// let a: number = 5;
+// let b: number = 2;
+// let c: number 
+// c =  ++a   +   a++   +    --b    +    b--   +     a     +      b++      +     b;                                                
+//       6    +    6    +     1     +    1     +     7     +       0       +     1
+//  let a: number = 12;
+//  let b: number =  8;
+//  let c: number
+//  c = ++a     +     a++    +     --b   +    b--     +     a    +   b++    +    b;
+//      13            13            7         7            14           6        7
+//  let a: number = 25;
+//  let b: number = 17;
+//  let c: number
+// c =  ++a     +     a++    +     --b   +    b--     +     a    +   b++     +      b;
+// //   26            26            16        16            27        15            16
+//  console.log(c);
+//   BEDMAS (PRACTICE)
+var num1 = 8;
+var num2 = 4;
+var num3 = 6;
+var num4 = 8;
+var num5 = 4;
+var total = 8 - 4 + (6 * 8 / 4);
+//8-4+(6*2)
+// 8-4+(12)
+//4+12=16
+// //          30   10   4   20   10 
+// //          30   10   4   2          
+// //          30   10   8    
+// //          30   18   
+// //          12
+console.log(total);
+// ASSIGNMENT OPERATORS(=)
+// let a = 10
+//  a = a + 5
+// console.log(a);
+//OR
+// let a = 10
+//     a += 5
+// console.log(a);
